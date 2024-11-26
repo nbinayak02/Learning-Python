e7ef5dfe626a424df67f5a3515ad2f2b855aa022
binayak-2 <binayakniraula2@gmail.com>
2024-11-26 21:55:22 +0545

    Learned Python Variables and Data types

---
+++ 01-Basics/06-DataTypes.py
@@ -0,0 +1,34 @@
+# numeric data type can be integer, floating number or complex number
+intA = int(23456)
+intB = -93485
+print("Integer Types =", intA, intB)
+
+# float can be positive, negative or scientific notation
+floatA = float(9234.45)
+floatB = -123.456
+floatC = 10e100 # 10 to the power of 100, float has limit
+print("Float Types = ", floatA, floatB, floatC)
+
+# complex number x+ij where x=real and j=imaginary
+cmplx = 4+23j
+print(type(cmplx), cmplx)
+
+#string data type is the object of str class
+strA = 'This is a string'
+strB = "This is also a string"
+strC = """And finally
+this is also a string"""
+strD = '''Opps my bad. This is 
+also a string'''
+print(strA)
+print(strB)
+print(strC)
+print(strD)
+
+# Sequence type - List, Tuple and Range
+# Binary Sequence type - bytes, bytesarray and memoryview
+# Set types - set, frozen set
+# Mapping types - dictionary
+# Boolean
+# None
+
